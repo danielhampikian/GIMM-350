89ce72055a06fa1839faf89c526407a23684da0e
Daniel Hampikian <danielhampikian@gmail.com>
2018-10-23 10:22:29 -0600

    added delegate and event sample code

---
+++ DesignPatterns/Delegate Pattern/DelegaterSubject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelegaterSubject : MonoBehaviour {
+
+	public delegate void MethodToDefine();
+    public static event MethodToDefine methodDefinedElsewhere;
+
+	void Start () {
+		
+	}
+    public void buttonClicked()
+    {
+        methodDefinedElsewhere();
+    }
+	
+}
---
+++ DesignPatterns/Delegate Pattern/RisingGameObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingGameObject : MonoBehaviour {
+
+    Animator anim;
+	void Start () {
+        anim = GetComponent<Animator>();
+        DelegaterSubject.methodDefinedElsewhere += makePlanetRise;
+        DelegaterSubject.methodDefinedElsewhere += logAnimationHappened;
+	}
+	
+    public void makePlanetRise()
+    {
+        anim.SetBool("isRising", true);
+        DelegaterSubject.methodDefinedElsewhere -= makePlanetRise;
+        DelegaterSubject.methodDefinedElsewhere += stopPlanetRise;
+    }
+    public void stopPlanetRise()
+    {
+        anim.SetBool("isRising", false);
+        DelegaterSubject.methodDefinedElsewhere -= stopPlanetRise;
+        DelegaterSubject.methodDefinedElsewhere += makePlanetRise;
+    }
+    public void logAnimationHappened()
+    {
+        Debug.Log("Hey, it worked");
+    }
+	
+	void Update () {
+		
+	}
+}
