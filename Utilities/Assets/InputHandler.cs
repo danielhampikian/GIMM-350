008184b6db2dcf6b2614fe3f865d163814e7731a
Daniel Hampikian <danielhampikian@gmail.com>
2019-09-12 09:38:52 -0600

    UTILTIES
    added begining of utilities project

---
+++ Utilities/Assets/InputHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public static class InputHandler
+{
+
+    public static string processText(string inS)
+    {
+       return inS;
+    }
+    public static int getAverageChars(string inS)
+    {
+        string[] words = inS.Split(' ');
+        int sum = 0;
+        foreach (var word in words)
+        {
+            sum += word.Length;
+        }
+        return sum / words.Length;
+    }
+    public static bool isNum(string inS)
+    {
+        try
+        {
+            double num = Double.Parse(inS);
+            return true;
+        }
+        catch (FormatException e)
+        {
+            Debug.Log(e);
+            return false;
+        }
+    }
+    public static bool betterIsNum(string inS)
+    {
+        if (Double.TryParse(inS, out double j))
+            return true;
+        else
+            return false;
+
+    }
+}
---
+++ Utilities/Assets/UILogic.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class UILogic : MonoBehaviour
+{
+    public Text text;
+    public string processedText;
+    public InputField input;
+
+    public void getInput()
+    
+    {
+        processText(input.textComponent.text);
+    }
+    public void displayOutput()
+    {
+        Debug.Log(processedText);
+        string typeOfInput = "none";
+        if (InputHandler.betterIsNum(processedText))
+        {
+            typeOfInput = "number";
+        }
+        else
+        {
+            typeOfInput = "string";
+        }
+        text.text = typeOfInput;
+        //text.text = InputHandler.getAverageChars(processedText).ToString();
+    }
+    public void processText(string inS)
+    {
+        Debug.Log("Before input handler: " + inS);
+        //this.processedText = inS;
+        this.processedText = InputHandler.processText(inS);
+        displayOutput();
+    }
+
+}
